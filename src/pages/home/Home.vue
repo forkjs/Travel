519d86c82285f7bbc4d93d5c358f661921df733e
forkjs <jforjs@163.com>
2019-03-20 16:07:53 +0800

    recommend and weekend

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -7,6 +7,8 @@
     <home-header></home-header>
     <home-swiper></home-swiper>
     <home-icons></home-icons>
+    <home-recommend></home-recommend>
+    <home-weekend></home-weekend>
   </div>
 </template>
 
@@ -14,6 +16,8 @@
   import HomeHeader from './components/Header'
   import HomeSwiper from './components/Swiper'
   import HomeIcons from './components/Icons'
+  import HomeRecommend from './components/Recommend'
+  import HomeWeekend from './components/Weekend'
 
   export default {
     name: "Home",
@@ -21,6 +25,8 @@
       HomeHeader,
       HomeSwiper,
       HomeIcons,
+      HomeRecommend,
+      HomeWeekend,
     }
   }
 </script>
--- src/pages/home/components/Icons.vue
+++ src/pages/home/components/Icons.vue
@@ -63,11 +63,6 @@
             imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/bd/9f7b9b2b60c1502.png',
             desc: '踏青赏花'
           },
-          {
-            id: '0009',
-            imgUrl: 'http://img1.qunarzz.com/piao/fusion/1803/47/c2b659e048b11602.png',
-            desc: '溜娃'
-          },
         ],
       }
     },
@@ -91,36 +86,37 @@
   @import "~style/varibles.styl"
   @import "~style/mixins.styl"
   .icons >>> .swiper-container
-    /*overflow hidden*/
     height 0
     padding-bottom 50%
-  .icon
-    position relative
-    overflow hidden
-    float left
-    width 25%
-    height 0
-    padding-bottom 25%
-    .icon-img
-      position absolute
-      top 0
-      left 0
-      right 0
-      bottom .44rem
-      box-sizing border-box
-      padding .1rem
-      .icon-img-content
-        display block
-        margin 0 auto
-        height 100%
-    .icon-desc
-      position absolute
-      left 0
-      right 0
-      bottom 0
-      height .44rem
-      line-height .44rem
-      color $darkTextColor
-      text-align center
-      ellipsis()
+  .icons
+    margin-top .1rem
+    .icon
+      position relative
+      overflow hidden
+      float left
+      width 25%
+      height 0
+      padding-bottom 25%
+      .icon-img
+        position absolute
+        top 0
+        left 0
+        right 0
+        bottom .44rem
+        box-sizing border-box
+        padding .1rem
+        .icon-img-content
+          display block
+          margin 0 auto
+          height 100%
+      .icon-desc
+        position absolute
+        left 0
+        right 0
+        bottom 0
+        height .44rem
+        line-height .44rem
+        color $darkTextColor
+        text-align center
+        ellipsis()
 </style>
