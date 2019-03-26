26d44f38319f28941b6e888f54d96bf105ae1120
forkjs <jforjs@163.com>
2019-03-26 09:42:33 +0800

    add city list

---
+++ src/pages/city/components/List.vue
@@ -0,0 +1,108 @@
+<template>
+  <div class="list" ref="wrapper">
+    <div>
+      <div class="area">
+        <div class="title border-topbottom">当前城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">热门城市</div>
+        <div class="button-list">
+          <div class="button-wrapper">
+            <div class="button">北京</div>
+          </div>
+        </div>
+      </div>
+      <div class="area">
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+        </div>
+        <div class="title border-topbottom">A</div>
+        <div class="item-list">
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+          <div class="item border-bottom">涂山</div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import BScroll from 'better-scroll'
+  export default {
+    name: "CityList",
+    mounted () {
+      this.scroll = new BScroll(this.$refs.wrapper)
+    },
+  }
+</script>
+
+<style scoped lang="stylus">
+  @import "~style/varibles.styl"
+  .border-topbottom
+    &:before
+      border-color #ccc
+    &:after
+      border-color #ccc
+  .border-bottom
+    &:before
+      border-color #ccc
+  .list
+    overflow hidden
+    position absolute
+    top 1.58rem
+    right 0
+    bottom 0
+    left 0
+    .title
+      line-height .54rem
+      padding-left .2rem
+      background #eee
+      color #666
+      font-size .26rem
+    .button-list
+      padding .1rem .6rem .1rem .1rem
+      overflow hidden
+      .button-wrapper
+        width 33.33%
+        float left
+        .button
+          text-align center
+          margin .1rem
+          padding .1rem 0
+          border .02rem solid #ccc
+          border-radius .06rem
+    .item-list
+
+      .item
+        line-height .76rem
+        padding-left .2rem
+</style>
