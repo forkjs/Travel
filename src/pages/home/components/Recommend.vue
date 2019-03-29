7e8b6b7d18c42cb8ce66fedefb06d5fedbd74a49
forkjs <jforjs@163.com>
2019-03-29 15:51:29 +0800

    add gallery

--- src/pages/home/components/Recommend.vue
+++ src/pages/home/components/Recommend.vue
@@ -2,18 +2,20 @@
   <div >
     <div class="title">热销推荐</div>
     <ul>
-      <li
-        class="item border-bottom"
-        v-for="item of list"
-        :key="item.id"
-      >
-        <img class="item-img" :src="item.imgUrl">
-        <div class="item-info">
-          <p class="item-title">{{item.title}}</p>
-          <p class="item-desc">{{item.desc}}</p>
-          <button class="item-button">查看详情</button>
-        </div>
-      </li>
+        <router-link
+          tag="li"
+          class="item border-bottom"
+          v-for="item of list"
+          :key="item.id"
+          :to="'/detail/' + item.id"
+        >
+          <img class="item-img" :src="item.imgUrl">
+          <div class="item-info">
+            <p class="item-title">{{item.title}}</p>
+            <p class="item-desc">{{item.desc}}</p>
+            <button class="item-button">查看详情</button>
+          </div>
+        </router-link>
     </ul>
   </div>
 </template>
@@ -24,30 +26,6 @@
     props: {
       list: Array
     },
-    // data () {
-    //   return {
-    //     recommendList: [
-    //       {
-    //         id: "0001",
-    //         imgUrl: "http://img1.qunarzz.com/sight/p0/1508/fe/434ee530e21f1c11bad088e8cc8d5e93.water.jpg_200x200_912ce9f4.jpg",
-    //         title: "鸟巢",
-    //         desc: "Bird\'s nest"
-    //       },
-    //       {
-    //         id: "0002",
-    //         imgUrl: "http://img1.qunarzz.com/sight/p0/1409/19/adca619faaab0898245dc4ec482b5722.jpg_200x200_1bc99086.jpg",
-    //         title: "故宫",
-    //         desc: "The Palace Museum"
-    //       },
-    //       {
-    //         id: "0003",
-    //         imgUrl: "http://img1.qunarzz.com/sight/p0/1603/ad/ad84d5339c3a34f390.water.jpg_200x200_788cb0d2.jpg",
-    //         title: "十渡风景区",
-    //         desc: "Shidu scenic spot"
-    //       },
-    //     ]
-    //   }
-    // }
   }
 </script>
 
